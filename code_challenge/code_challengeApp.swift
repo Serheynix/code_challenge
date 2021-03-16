5b0f8c848212f8feec8dd6e52c77205afb25ee3e
Serhii Semenov <nkptpro@gmail.com>
2021-03-16 23:17:07 +0200

    Initial Commit

---
+++ code_challenge/code_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_challengeApp.swift
+//  code_challenge
+//
+//  Created by Serhii Semenov on 16.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct code_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
